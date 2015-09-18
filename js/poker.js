a7b8993274655b7f45c52360d668481673dadd88
vani_junying@hotmail.com <vali@SSC1-W257.schenker-asia.com>
2015-09-18 17:44:44 +0800

    rename file name, and preparing for next step

--- js/poker.js
+++ js/poker.js
@@ -6,16 +6,6 @@
  * To change this template use File | Settings | File Templates.
  */
 $(function() {
-    pokerFunctions.generatingPokers();
-});
-var number = ['A','2','3','4','5','6','7','8','9','10','J','Q','K'];
-var color = []
-var pokerFunctions = {
-    generatingPokers:function(){
-      var htmlEles = '';
-        //随机发五张牌
-        for(var i=0;i<=4;i++){
 
-        }
-    }
-}
+});
+
